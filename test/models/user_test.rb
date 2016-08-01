52e5075f823bac71ee437f5667db68e434eb9ce7
Jonathon Carlson <jcarlson@verisys.com>
2016-08-01 14:24:15 -0600

    added user, with auth and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,10 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: 'Bob', email: 'bob@bob.com')
+    @user = User.new(name: 'Bob', email: 'bob@bob.com', password: "APassword", password_confirmation: "APassword")
   end
 
-  test "Shoudl be valid" do
+  test "should be valid" do
     assert @user.valid?
   end
 
@@ -33,7 +33,31 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email address should be valid" do
-    valid_addresses = %w[user@example.com User@u.com A_u.s-e+r@u.cn b@some.net new@user.org ]
+    valid_addresses = %w[user@example.com User@u.COM A_u.s-e+r@u.cn b@some.net new@user.org another@blah.co]
+    valid_addresses.each do |email_address|
+      @user.email = email_address
+      assert @user.valid?, "#{email_address} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example user_at_foo.org new@new_site.com extra@new+site.com]
+    invalid_addresses.each do |email_address|
+      @user.email = email_address
+      assert_not @user.valid?, "#{email_address} should not be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
